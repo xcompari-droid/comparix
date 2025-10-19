bd15c8164832dd0e7c621d30dce25bd6ccc9367e
xcompari-droid <xcompari@gmail.com>
2025-10-19 22:03:36 +0300

    Remove ping route and clean up web.php

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', fn () => 'pong-' . now());
-
 Route::get('/', fn () => view('welcome'));
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
