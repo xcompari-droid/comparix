2f93073e6c86c00684a7fb60e3cca164ba00fea7
xcompari-droid <xcompari@gmail.com>
2025-10-19 18:38:01 +0300

    Remove comment and clean up welcome route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-// TEMP de verificare – poți lăsa direct welcome dacă vrei
 Route::get('/', function () {
-    return view('welcome'); // ← nu 'home'
+    return view('welcome');
 });
 
